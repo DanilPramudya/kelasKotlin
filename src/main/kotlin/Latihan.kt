69413762ecbbe9c9c5e45ce135622748addcaeca
Danil Pramudya <danielpramudya@gmail.com>
2022-10-04 16:25:04 +0700

    latihan-1

--- src/main/kotlin/Latihan.kt
+++ src/main/kotlin/Latihan.kt
@@ -1,22 +1,22 @@
 fun main() {
-
     var lamaParkir: Int
     var biayaParkir: Float
 
-    // rule biaya parkir
+    lamaParkir = 72
 
-    lamaParkir = 8
-
+    //aturan bayar parkir
     val tarifDasar = 1.0
     val tarifBerikutnya = 0.5
     val maxTarifPerHari = 15
 
     if (lamaParkir <= 5) {
-        biayaParkir = ((lamaParkir * tarifDasar).toFloat())
+        biayaParkir = (lamaParkir * tarifDasar).toFloat()
     } else if ((lamaParkir > 5) && (lamaParkir < 24)) {
         biayaParkir = (5 + ((lamaParkir-5)*tarifBerikutnya)).toFloat()
+    } else if ((lamaParkir%24)==0) {
+        biayaParkir = (maxTarifPerHari * (lamaParkir/24)).toFloat()
     } else {
-        biayaParkir = maxTarifPerHari.toFloat()
+        biayaParkir = (15 + ((lamaParkir-24)*tarifBerikutnya)).toFloat()
     }
 
     println("Lama Parkir: $lamaParkir")
